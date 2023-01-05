138b388b0b205de60f76f98176c1fe6a742184a2
besufikad17 <besufikadtialhun1994@gmail.com>
2023-01-05 21:56:40 +0300

    basic UI components implemented

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+  import Header from "./lib/Header.svelte";
+
+  const data = [1,2,3,4];
+  let newData = [];
+  for(let i = 0;i < data.length; i++){
+    if(i % 2 !== 0){
+      newData.push([data[i - 1], data[i]])
+    }
+  }
+
+</script>
+
+<main>
+  <Header /><br/>
+
+  <div class="parent">
+    {#each newData as d}
+        <div class="row">
+          <div class="child">{d[0]}</div>
+          <div class="child">{d[1]}</div>
+        </div>
+    {/each}
+  </div>
+</main>
+
+<style>
+  .parent {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .child {
+    width: 100%;
+    height: 100px;
+    background-color: #a8a4a4;
+    color: white;
+    text-align: center;
+    margin: 10px;
+  }
+
+  @media (max-width: 768px) {
+    .parent {
+      flex-direction: column;
+    }
+
+    .row {
+      flex-direction: column;
+    }
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+    import android from "../assets/imgs/icons8-android-96.png";
+    import github from "../assets/imgs/icons8-github-48.png";
+</script>
+
+<main>
+    <div class="parent">
+        <div class="logo">
+          <!-- FIXME get decent logo -->
+          <img src="" alt="logo" />
+        </div>
+        <div class="links">
+          <a href="/"><img src={android} alt="android app" /></a>
+          <a href="/"><img src={github} alt="github" /></a>
+        </div>
+    </div>
+</main>
+  
+<style>
+  .parent {
+    width: auto;
+    height: 50%;
+    overflow: hidden;
+    padding: 20px 10px;
+    background-color: rgb(255, 255, 255);
+  }
+  .logo {
+    float: left;
+    margin-left: 10px;
+  }
+  .links {
+    float: right;
+  }
+  .links a {
+    text-decoration: none;
+    color: white;
+    text-align: center;
+    padding: 5px;
+  }
+  .links a img {
+    width: 46px;
+    height: 46px;
+  }
+</style>
