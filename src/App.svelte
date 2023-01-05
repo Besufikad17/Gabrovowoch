dfb46d43a911703e38bc4244417892920e90d488
besufikad17 <besufikadtialhun1994@gmail.com>
2023-01-05 22:47:23 +0300

    responsive card implemented

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,32 @@
 <script>
   import Header from "./lib/Header.svelte";
+  import Card from "./lib/Card.svelte";
 
-  const data = [1,2,3,4];
+  const data = [1, 2, 3, 4];
   let newData = [];
-  for(let i = 0;i < data.length; i++){
-    if(i % 2 !== 0){
-      newData.push([data[i - 1], data[i]])
+  for (let i = 0; i < data.length; i++) {
+    if (i % 2 !== 0) {
+      newData.push([data[i - 1], data[i]]);
     }
   }
-
 </script>
 
 <main>
-  <Header /><br/>
+  <Header /><br />
 
   <div class="parent">
-    {#each newData as d}
+    <div class="container">
+      {#each newData as d}
         <div class="row">
-          <div class="child">{d[0]}</div>
-          <div class="child">{d[1]}</div>
+          <div class="child">
+            <Card title="test" description={"" + d[0]} />
+          </div>
+          <div class="child">
+            <Card title="test" description={"" + d[1]} />
+          </div>
         </div>
-    {/each}
+      {/each}
+    </div>
   </div>
 </main>
 
@@ -30,18 +36,26 @@
     flex-direction: column;
   }
 
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
   .row {
     display: flex;
     flex-direction: row;
   }
 
   .child {
-    width: 100%;
-    height: 100px;
+    width: auto;
+    min-width: 400px;
+    height: 300px;
     background-color: #a8a4a4;
     color: white;
-    text-align: center;
+    text-align: left;
     margin: 10px;
+    border-radius: 8px;
   }
 
   @media (max-width: 768px) {
---
+++ src/lib/Card.svelte
@@ -0,0 +1,38 @@
+<script>
+  import like from "../assets/imgs/icons8-facebook-like-90.png";
+  
+  // FIXME props
+  export const title = "";
+  export const description = "";
+  let likes = 0;
+</script>
+
+<main>
+  <div class="parent">
+     <h2>{title}</h2>
+     <p>{description}</p>
+     <div class="action-center">
+        <button on:click={() => likes++}><img src={like} alt="like"/>{" " + likes}</button>
+     </div>
+  </div>
+</main>
+
+<style>
+    .action-center {
+        float: right;
+    }
+
+    .action-center button {
+        border-radius: 15px;
+        background-color: #897070;
+        border-style: none;
+        align-items: center;
+        text-align: center;
+        margin-right: 10px;
+    }
+
+    .action-center button img {
+        width: 25px;
+        height: 25px;
+    }
+</style>
