8ebdb87c19b238d4286c2eec066fd7d832286f01
besufikad17 <besufikadtialhun1994@gmail.com>
2023-01-27 17:08:42 +0300

    change layout option added

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,36 +1,176 @@
 <script>
+  import Header from "./Header.svelte";
   import Card from "./Card.svelte";
-  import { onMount } from "svelte";
-  import axios from "axios";
+  import Footer from "./Footer.svelte";
+  import Joke from "./Joke.svelte";
 
+  import Icon from "@iconify/svelte";
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+
+  let isLoading = true;
   let isGrid = true;
   let data = [];
   let datas = [];
+  let skip = 0;
+  let take = 10;
 
+  const fetch = async () => {
+    axios
+      .get(
+        `https://gabrovowoch-backend.onrender.com/all?skip=${skip}&take=${take}&api_key=a6803db0`
+      )
+      .then((result) => {
+        if (result.data.success) {
+          data = result.data.data;
+          let newDatas = [];
+          for (let i = 0; i < data.length; i++) {
+            if (i % 2 != 0) {
+              newDatas.push([data[i - 1], data[i]]);
+            }
+          }
+          datas = [...newDatas];
+        }
+      })
+      .catch((e) => {
+        throw e;
+      });
+  };
+
+  const next = async () => {
+    skip = skip + 10;
+    fetch();
+  };
+
+  const previous = async () => {
+    skip = skip - 10;
+    fetch();
+  };
+
+  onMount(async () => {
+    fetch();
+    isLoading = false;
+  });
 </script>
-   {#if isGrid}
-    <div class="parent">
-        <div class="container">
-        {#each datas as d}
-            <div class="row">
-            <div class="child">
-                <Card title={d[0].title} description={"" + d[0].description} likes={0} />
-            </div>
-            <div class="child">
-                <Card title={d[1].title} description={"" + d[1].description} likes={0}/>
-            </div>
-            </div>
-        {:else}
-            <p>loading...</p>
-        {/each}
-        </div>
-    </div>
-   {/if}
-   
-<main>
 
+<main>
+  <Header /><br />
+
+  <div class="parent">
+    <div class="container">
+      <br />
+      <button class="navigator" on:click={()=> (isGrid = !isGrid)}>
+        {#if isGrid}
+            <Icon icon="ic:baseline-check-box-outline-blank" />
+        {:else}
+            <Icon icon="material-symbols:grid-view-outline" />
+        {/if}
+        </button
+      ><br />
+
+      {#if isGrid}
+        {#each datas as d}
+          <div class="row">
+            <div class="child">
+              <Card
+                title={d[0].title}
+                description={"" + d[0].description}
+                likes={0}
+              />
+            </div>
+            <div class="child">
+              <Card
+                title={d[1].title}
+                description={"" + d[1].description}
+                likes={0}
+              />
+            </div>
+          </div>
+        {:else}
+          <p>loading...</p>
+        {/each}
+
+        {#if !isLoading}
+          <div class="action-row">
+            {#if skip > 10}
+              <button on:click={previous} class="navigator">
+                <Icon icon="ic:round-keyboard-arrow-left" />
+              </button>
+            {/if}
+            <button on:click={next} class="navigator">
+              <Icon icon="ic:round-keyboard-arrow-right" />
+            </button>
+          </div>
+        {/if}
+      {:else}
+        <!-- <Joke /> -->
+      {/if}
+    </div>
+  </div>
+  <br />
+
+  {#if !isLoading}
+    <Footer />
+  {/if}
 </main>
 
 <style>
+  .parent {
+    display: flex;
+    flex-direction: column;
+  }
 
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .action-row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .child {
+    width: auto;
+    min-width: auto;
+    background-color: #d5e7dc;
+    text-align: left;
+    margin: 10px;
+    border-radius: 8px;
+  }
+
+  .navigator {
+    border-radius: 5px;
+    display: flex;
+    background-color: white;
+    color: #897070;
+    border-style: solid 1px;
+    border-color: #897070;
+    align-items: center;
+    text-align: center;
+    margin-right: 10px;
+    font-size: 24px;
+  }
+
+  .navigator:hover {
+    background-color: #897070;
+    color: white;
+  }
+
+  @media (max-width: 768px) {
+    .parent {
+      flex-direction: column;
+    }
+
+    .row {
+      flex-direction: column;
+    }
+  }
 </style>
--- src/lib/Joke.svelte
+++ src/lib/Joke.svelte
@@ -1,32 +1,87 @@
 <script>
-   import Card from "./Card.svelte";
-   
-   const urlParams = new URLSearchParams(window.location.search);
-   const id = urlParams.get("id");
-   let joke;
+  import { onMount } from "svelte";
+  import Card from "./Card.svelte";
+  import axios from "axios";
+  import Header from "./Header.svelte";
+  
+  export var id;
+  let data = null;
 
-   
-
+  onMount(async () => {
+    axios
+      .get(
+        `https://gabrovowoch-backend.onrender.com/joke/${id}?api_key=a6803db0`
+      )
+      .then((result) => {
+        console.log(result);
+        if (result.data.success) {
+          data = result.data.data;
+        }
+      })
+      .catch((e) => {
+        throw e;
+      });
+  });
 </script>
 
 <main>
+  <Header /><br />
   <div class="parent">
-    <div class="row">
-      <div class="child">
-        <Card title={joke.title} description={joke.description} likes={joke.likes} /> 
+    <div class="container">
+      <div class="row">
+        <div class="child">
+          {#if data}
+            <Card
+              title={data.title}
+              description={data.description}
+              likes={data.likes}
+            />
+          {/if}
+        </div>
       </div>
     </div>
+  </div>
+  <br />
+  <div class="footer">
+    <p>@2023 ጋቭሮቮዎች</p>
   </div>
 </main>
 
 <style>
-   .parent {
-      display: flex;
-      flex-direction: column;
-    }
+  .parent {
+    display: flex;
+    flex-direction: column;
+  }
 
-    .row {
-      display: flex;
-      flex-direction: column;
-    }
+  .row {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .child {
+    width: auto;
+    min-width: auto;
+    background-color: #d5e7dc;
+    text-align: left;
+    margin: 10px;
+    border-radius: 8px;
+  }
+
+  .footer {
+    justify-content: center;
+    padding: 10px;
+    bottom: 0px;
+    text-align: center;
+    height: 50px;
+    color: white;
+    background-color: #626034;
+    margin-top: 273px;
+  }
 </style>
